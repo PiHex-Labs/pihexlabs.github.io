7e1fcb35d08f94d6f95c40bff3ab894443baa535
Amit Kumar <dtu.amit@gmail.com>
2025-01-06 19:19:17 +0000

    fix base url and add readme

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pihex-labs/', // Replace with your repository name
+  base: '/', // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
